48a4486cdccf2910b06fcb302bbfcf3771c6fc9f
Lucas Rodriguez <lucasg29r95@gmail.com>
2022-07-25 03:01:54 -0300

    Se actualizo el componente Footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Contacto } from '../models/contacto';
 import { ContactoService } from '../service/contacto.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,7 @@ import { ContactoService } from '../service/contacto.service';
 export class FooterComponent implements OnInit {
 
   @Input() conts: Contacto[] = [];
+  @Input() permis: boolean = false;
 
   constructor(
     private contactoService: ContactoService
@@ -21,7 +23,17 @@ export class FooterComponent implements OnInit {
         this.conts = data;
       },
       err => {
-        alert("Error! " + err.message);
+        Swal.fire({
+          text: 'Error al cargar datos: ' + err.message,
+          icon: 'error',
+          iconColor: '#ddd',
+          position: 'top-end',
+          background: '#c43725',
+          color: '#ddd',
+          width: 300,
+          showConfirmButton: false,
+          timer: 1500
+        });
       }
     );
   }
