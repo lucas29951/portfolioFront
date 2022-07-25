817495be3cbb29a8674b09df76e781904422f0bd
Lucas Rodriguez <lucasg29r95@gmail.com>
2022-07-25 03:09:09 -0300

    Se actualizo el componente Header con la tecnologia JWT.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,10 +12,7 @@ import { TokenService } from '../service/token.service';
 export class HeaderComponent implements OnInit {
 
   isLogged = false;
-  nombreUsuario: string = '';
-  password: string = '';
   loginUsuario: LoginUsuario = new LoginUsuario('','');
-  roles: string[]= [];
 
   constructor(
     private router: Router,
@@ -37,20 +34,4 @@ export class HeaderComponent implements OnInit {
     window.location.reload();
   }
 
-  onLogin(): void {
-    this.loginUsuario = new LoginUsuario(this.nombreUsuario,this.password);
-    this.authService.login(this.loginUsuario).subscribe(
-      data => {
-        this.isLogged = true;
-        this.tokenService.setToken(data.token);
-        this.tokenService.setUserName(data.nombreUsuario);
-        this.tokenService.setAuthorities(data.authorities);
-        this.roles = data.authorities;
-        window.location.reload();
-      },
-      err => {
-        this.isLogged = false;
-      }
-    );
-  }
 }
